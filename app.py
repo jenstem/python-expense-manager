d56ef0c49353d572b901ca4de55c013aa9a7b082
jenstem <jennifer.stemkowski@gmail.com>
2024-04-30 15:52:00 -0400

    Add expenses to DB

--- app.py
+++ app.py
@@ -34,6 +34,9 @@ def addexpense():
     amount = request.form['amount']
     category = request.form['category']
     print(date, expensename, amount, category)
+    expense = Expense(date=date, expensename=expensename, amount=amount, category=category)
+    db.session.add(expense)
+    db.session.commit()
     return redirect('/')
 
 
