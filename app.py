b37e685b1b31d9663ac17e2161c3ff5e4f5e89d0
jenstem <jennifer.stemkowski@gmail.com>
2024-04-30 15:21:21 -0400

    Add add-expense functionality

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def add():
     return render_template('add.html')
+
+@app.route('/addexpense', methods=['POST'])
+def addexpense():
+    date = request.form['date']
+    expensename = request.form['expensename']
+    amount = request.form['amount']
+    category = request.form['category']
+    print(date, expensename, amount, category)
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
