21b4cdd8cfb6e97795c8016c2cbe72a005b35ad0
jenstem <jennifer.stemkowski@gmail.com>
2024-04-30 15:47:25 -0400

    Create database

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "mydatabase.db"))
+
 
 app = Flask(__name__)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(80), unique=False, nullable=False)
+    expensename = db.Column(db.String(80), unique=False, nullable=False)
+    amount = db.Column(db.Integer, unique=False, nullable=False)
+    category = db.Column(db.String(80), unique=False, nullable=False)
+
 
 @app.route('/')
 def add():
     return render_template('add.html')
+
 
 @app.route('/addexpense', methods=['POST'])
 def addexpense():
